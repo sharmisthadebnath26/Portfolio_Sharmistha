4aa32c0eeb1c51d07b3b5b164bda70c3d121a764
Sharmistha <riyasamirdebnath@gmail.com>
2023-10-09 15:03:36 +0530

    Home and About page is now mobile, tablet and desktop responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,8 @@ import { Box, Button } from '@mui/material'
 import './About.css'
 import Self from './self.png';
 import MediaQuery from 'react-responsive'
-
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 export default function () {
@@ -13,6 +14,10 @@ export default function () {
     const [hover, setHover] = useState(true);
     const [hoverUnderline2, setHoverUnderline2] = useState(true);
     const [hover1, setHover2] = useState(true);
+
+    //responsive
+    const [bar, setbar] = useState(true);
+
     const handleHover = () => {
         setHover(false);
     }
@@ -33,7 +38,12 @@ export default function () {
     }
 
     //responsive
-    
+    const navbarOpen = () => {
+        setbar(false);
+    }
+    const closeNavbar = () => {
+        setbar(true);
+    }
 
   return (
     <>
@@ -67,17 +77,189 @@ export default function () {
             and a solid understanding of web development principles. Quick learner, eager to contribute innovative solutions to create seamless user experiences. 
             Open to challenges and seeking opportunities to leverage creativity and technical expertise in a growth-oriented environment.<br/>
             &#10024; Available for Freelancing <br/> &#128640; Let's collaborate and bring your web projects to life!</span><br/>
-            <a href='https://github.com/sharmisthadebnath26' style={{color:'#ffffff', margin:'auto'}} target='_blank'><Button variant="outlined" 
+            <a href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:101abd32-e2d7-3d9c-ac9a-4a0ecc2068f8' style={{color:'#ffffff', textDecoration:'none'}} target='_blank'><Button variant="outlined" 
         onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
-        style={hover1 ? {color:"#070B2B", background:'#ffffff', margin:'25px auto'} : {borderColor:'#ffffff', color:'#ffffff', margin:'25px auto'}}>
-        Github Profile
+        style={hover1 ? {color:"#070B2B", background:'#ffffff', margin:'25px 0'} : {display:'block', borderColor:'#ffffff', color:'#ffffff', margin:'25px 0'}}>
+        My Resume
         </Button></a>
         </Box>
         </Box>
     </MediaQuery>
-    <MediaQuery minWidth={1224}>
-
+    <MediaQuery minWidth={1358} maxWidth={1471}>
+    <Box style={{display:'flex', justifyContent:'space-between'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'flex', padding:20}}>
+            <Box style={{margin:'20px 10px'}}>
+                <a style={{textDecoration:'underline', color:'#ffffff'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#ffffff'} : {textDecoration:'underline', color:'#ffffff'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button className='button' variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#ffffff', color:'#ffffff', margin:'13px'}:{color:"#070B2B", background:'#ffffff', margin:'13px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        <Box style={{display:'flex', justifyContent:'space-between'}}>
+            <img src={Self} alt="Self" style={{margin:'100px 25px 70px 70px', width:'400px', height:'400px'}}/>
+            <Box style={{margin:'80px 15px 30px 15px'}}>
+            <h1 style={{color:'#ffffff', margin:'20px 0'}}>Hi,</h1>
+            <span style={{color:'#ffffff', lineHeight:'40px', fontSize:'22px'}}>I am <b>Sharmistha Debnath</b>, passionate <em>Frontend Developer</em>. I am proficient in creating dynamic and responsive user interfaces, with a keen eye for design aesthetics 
+            and a solid understanding of web development principles. Quick learner, eager to contribute innovative solutions to create seamless user experiences. 
+            Open to challenges and seeking opportunities to leverage creativity and technical expertise in a growth-oriented environment.<br/>
+            &#10024; Available for Freelancing <br/> &#128640; Let's collaborate and bring your web projects to life!</span><br/>
+            <a href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:101abd32-e2d7-3d9c-ac9a-4a0ecc2068f8' style={{color:'#ffffff', textDecoration:'none'}} target='_blank'><Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {color:"#070B2B", background:'#ffffff', margin:'25px 0'} : {borderColor:'#ffffff', color:'#ffffff', margin:'25px 0'}}>
+        My Resume
+        </Button></a>
+        </Box>
+        </Box>
     </MediaQuery>
+
+    <MediaQuery minWidth={900} maxWidth={1358} >
+    <Box style={{display:'flex', justifyContent:'space-between', maxHeight:'100px'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'block'}}>
+        <MenuIcon style={{color:'#ffffff', margin:'35px 40px', fontSize:'35px', float:'right'}} onClick={navbarOpen} />
+        <Box style={{backgroundColor:'#ffffff', display:'block', minwidth:'30%', maxWidth:'70%', marginRight:'10px', top:'20px', right:'0px'}} position="absolute">
+        <CloseIcon onClick={closeNavbar} style={bar ? {display:'none'} : {float:'right', color:'#121A2D', margin:'10px'}}/>
+        <Box style={bar ? {display:'none', padding:'10px'} : {display:'flex', flexDirection:'column', padding:'10px'}}>
+            <Box style={{margin:'30px 10px 10px 5px'}}>
+                <a style={{textDecoration:'underline', color:'#121A2D'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#121A2D'} : {textDecoration:'underline', color:'#121A2D'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#121A2D', color:'#121A2D', margin:'10px', fontSize:'15px'}:{color:"#ffffff", background:'#121A2D', margin:'10px', fontSize:'15px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        </Box>
+        
+        </Box>
+        
+            <img src={Self} alt="Self" style={{display:'block', margin:'100px auto 50px auto', width:'250px', height:'280px'}}/>
+            <Box>
+            <h1 style={{color:'#ffffff', margin:'10px 25px 15px 25px'}}>Hi,</h1>
+            <p style={{color:'#ffffff', lineHeight:'40px', fontSize:'25px', margin:'5px 25px'}}>I am <em>Sharmistha Debnath</em>, passionate <b>Frontend Developer</b>. I am proficient in creating dynamic and responsive user interfaces, with a keen eye for design aesthetics 
+            and a solid understanding of web development principles. Quick learner, eager to contribute innovative solutions to create seamless user experiences. 
+            Open to challenges and seeking opportunities to leverage creativity and technical expertise in a growth-oriented environment.<br/>
+            &#10024; Available for Freelancing <br/> &#128640; Let's collaborate and bring your web projects to life!</p><br/>
+            <a href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:101abd32-e2d7-3d9c-ac9a-4a0ecc2068f8' style={{color:'#ffffff', textDecoration:'none'}} target='_blank' rel="noreferrer"><Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {display:'block', color:"#070B2B", background:'#ffffff', margin:'30px auto 25px auto'} : {display:'block', borderColor:'#ffffff', color:'#ffffff', margin:'10px auto 25px auto'}}>
+        My Resume
+        </Button></a>
+        </Box>
+    </MediaQuery>
+
+    <MediaQuery minWidth={320} maxWidth={900} >
+    <Box style={{display:'flex', justifyContent:'space-between', maxHeight:'100px'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'block'}}>
+        <MenuIcon style={{color:'#ffffff', margin:'35px 40px', fontSize:'35px', float:'right'}} onClick={navbarOpen} />
+        <Box style={{backgroundColor:'#ffffff', display:'block', minwidth:'30%', maxWidth:'70%', marginRight:'10px', top:'20px', right:'0px'}} position="absolute">
+        <CloseIcon onClick={closeNavbar} style={bar ? {display:'none'} : {float:'right', color:'#121A2D', margin:'10px'}}/>
+        <Box style={bar ? {display:'none', padding:'10px'} : {display:'flex', flexDirection:'column', padding:'10px'}}>
+            <Box style={{margin:'30px 10px 10px 5px'}}>
+                <a style={{textDecoration:'underline', color:'#121A2D'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#121A2D'} : {textDecoration:'underline', color:'#121A2D'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#121A2D', color:'#121A2D', margin:'10px', fontSize:'15px'}:{color:"#ffffff", background:'#121A2D', margin:'10px', fontSize:'15px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        </Box>
+        
+        </Box>
+        
+            <img src={Self} alt="Self" style={{display:'block', margin:'50px auto', width:'250px', height:'280px'}}/>
+            <Box>
+            <h1 style={{color:'#ffffff', margin:'10px 25px 15px 25px'}}>Hi,</h1>
+            <p style={{color:'#ffffff', lineHeight:'40px', fontSize:'18px', margin:'5px 25px'}}>I am <em>Sharmistha Debnath</em>, passionate <b>Frontend Developer</b>. I am proficient in creating dynamic and responsive user interfaces, with a keen eye for design aesthetics 
+            and a solid understanding of web development principles. Quick learner, eager to contribute innovative solutions to create seamless user experiences. 
+            Open to challenges and seeking opportunities to leverage creativity and technical expertise in a growth-oriented environment.<br/>
+            &#10024; Available for Freelancing <br/> &#128640; Let's collaborate and bring your web projects to life!</p><br/>
+            <a href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:101abd32-e2d7-3d9c-ac9a-4a0ecc2068f8' style={{color:'#ffffff', textDecoration:'none'}} target='_blank' rel="noreferrer"><Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {display:'block', color:"#070B2B", background:'#ffffff', margin:'10px auto 25px auto'} : {display:'block', borderColor:'#ffffff', color:'#ffffff', margin:'10px auto 25px auto'}}>
+        My Resume
+        </Button></a>
+        </Box>
+    </MediaQuery>
+    <MediaQuery minWidth={270} maxWidth={320} >
+    <Box style={{display:'flex', justifyContent:'space-between', maxHeight:'100px'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'block'}}>
+        <MenuIcon style={{color:'#ffffff', margin:'35px 40px', fontSize:'35px', float:'right'}} onClick={navbarOpen} />
+        <Box style={{backgroundColor:'#ffffff', display:'block', minwidth:'30%', maxWidth:'70%', marginRight:'10px', top:'20px', right:'0px'}} position="absolute">
+        <CloseIcon onClick={closeNavbar} style={bar ? {display:'none'} : {float:'right', color:'#121A2D', margin:'10px'}}/>
+        <Box style={bar ? {display:'none', padding:'10px'} : {display:'flex', flexDirection:'column', padding:'10px'}}>
+            <Box style={{margin:'30px 10px 10px 5px'}}>
+                <a style={{textDecoration:'underline', color:'#121A2D'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#121A2D'} : {textDecoration:'underline', color:'#121A2D'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#121A2D', color:'#121A2D', margin:'10px', fontSize:'15px'}:{color:"#ffffff", background:'#121A2D', margin:'10px', fontSize:'15px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        </Box>
+        
+        </Box>
+        
+            <img src={Self} alt="Self" style={{display:'block', margin:'50px auto', width:'250px', height:'280px'}}/>
+            <Box>
+            <h1 style={{color:'#ffffff', margin:'10px 25px 15px 25px'}}>Hi,</h1>
+            <p style={{color:'#ffffff', lineHeight:'40px', fontSize:'18px', margin:'5px 25px'}}>I am <em>Sharmistha Debnath</em>, passionate <b>Frontend Developer</b>. I am proficient in creating dynamic and responsive user interfaces, with a keen eye for design aesthetics 
+            and a solid understanding of web development principles. Quick learner, eager to contribute innovative solutions to create seamless user experiences. 
+            Open to challenges and seeking opportunities to leverage creativity and technical expertise in a growth-oriented environment.<br/>
+            &#10024; Available for Freelancing <br/> &#128640; Let's collaborate and bring your web projects to life!</p><br/>
+            <a href='https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:101abd32-e2d7-3d9c-ac9a-4a0ecc2068f8' style={{color:'#ffffff', textDecoration:'none'}} target='_blank' rel="noreferrer"><Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {display:'block', color:"#070B2B", background:'#ffffff', margin:'10px auto 25px auto'} : {display:'block', borderColor:'#ffffff', color:'#ffffff', margin:'10px auto 25px auto'}}>
+        My Resume
+        </Button></a>
+        </Box>
+    </MediaQuery>
+
+
+
      
         
         
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
 import { Box, Button } from '@mui/material'
 import './Home.css'
+import MediaQuery from 'react-responsive'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Home() {
     const [hover, setHover] = useState(true);
     const [hover1, setHover2] = useState(true);
     const [hoverUnderline1, setHoverUnderline1] = useState(true);
     const [hoverUnderline2, setHoverUnderline2] = useState(true);
+
+    //responsive
+    const [bar, setbar] = useState(true);
+
+
     const handleHover = () => {
         setHover(false);
     }
@@ -32,9 +40,20 @@ export default function Home() {
     const handleHoverUnderlineLeft2 = () => {
         setHoverUnderline2(true);
     }
+
+    //responsive
+    const navbarOpen = () => {
+        setbar(false);
+    }
+    const closeNavbar = () => {
+        setbar(true);
+    }
+
+
   return (
     <>
-        <Box style={{display:'flex', justifyContent:'space-between'}}>
+    <MediaQuery minWidth={900}>
+    <Box style={{display:'flex', justifyContent:'space-between'}}>
         <Box>
             <h2 className='myName'>Sharmistha Debnath</h2>
         </Box> 
@@ -58,7 +77,47 @@ export default function Home() {
         
         </Box>
 
-        <h1 style={{color:'#ffffff', fontSize:70, textAlign:'center', marginTop:'12%', display:'grid'}}>Weaving Dreams into Web Reality</h1>
+        <h1 style={{color:'#ffffff', fontSize:70, textAlign:'center', margin:'100px 15px 40px 15px', display:'grid'}}>Weaving Dreams into Web Reality</h1>
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {color:"#070B2B", background:'#ffffff', margin:'10px auto'} : {borderColor:'#ffffff', color:'#ffffff', margin:'25px auto'}}>
+        Dig into my universe
+        </Button>
+    </div>
+    </MediaQuery>
+        
+
+    <MediaQuery minWidth={320} maxWidth={900} >
+    <Box style={{display:'flex', justifyContent:'space-between', maxHeight:'100px'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'block'}}>
+        <MenuIcon style={{color:'#ffffff', margin:'35px 40px', fontSize:'35px', float:'right'}} onClick={navbarOpen} />
+        <Box style={{backgroundColor:'#ffffff', display:'block', minwidth:'30%', maxWidth:'70%', marginRight:'10px', top:'20px', right:'0px'}} position="absolute">
+        <CloseIcon onClick={closeNavbar} style={bar ? {display:'none'} : {float:'right', color:'#121A2D', margin:'10px'}}/>
+        <Box style={bar ? {display:'none', padding:'10px'} : {display:'flex', flexDirection:'column', padding:'10px'}}>
+            <Box style={{margin:'30px 10px 10px 5px'}}>
+                <a style={{textDecoration:'underline', color:'#121A2D'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#121A2D'} : {textDecoration:'underline', color:'#121A2D'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#121A2D', color:'#121A2D', margin:'10px', fontSize:'15px'}:{color:"#ffffff", background:'#121A2D', margin:'10px', fontSize:'15px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        </Box>
+        
+        </Box>
+        <h2 style={{color:'#ffffff', fontSize:70, textAlign:'center', margin:'100px 20px 25px 20px', display:'grid'}}>Weaving Dreams into Web Reality</h2>
     <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
         <Button variant="outlined" 
         onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
@@ -66,7 +125,46 @@ export default function Home() {
         Dig into my universe
         </Button>
     </div>
-
+        </MediaQuery>
+        
+        <MediaQuery minWidth={280} maxWidth={320} >
+    <Box style={{display:'flex', justifyContent:'space-between', maxHeight:'100px'}}>
+        <Box>
+            <h2 className='myName'>Sharmistha Debnath</h2>
+        </Box> 
+        <Box style={{display:'block'}}>
+        <MenuIcon style={{color:'#ffffff', margin:'35px 40px', fontSize:'35px', float:'right'}} onClick={navbarOpen} />
+        <Box style={{backgroundColor:'#ffffff', display:'block', minwidth:'30%', maxWidth:'70%', marginRight:'10px', top:'20px', right:'0px'}} position="absolute">
+        <CloseIcon onClick={closeNavbar} style={bar ? {display:'none'} : {float:'right', color:'#121A2D', margin:'10px'}}/>
+        <Box style={bar ? {display:'none', padding:'10px'} : {display:'flex', flexDirection:'column', padding:'10px'}}>
+            <Box style={{margin:'30px 10px 10px 5px'}}>
+                <a style={{textDecoration:'underline', color:'#121A2D'}}>ABOUT</a>
+            </Box>
+            <Box style={{margin:'20px 10px'}}>
+                <a onMouseEnter={handleHoverUnderline2} onMouseLeave={handleHoverUnderlineLeft2} href='#' 
+                style={hoverUnderline2 ? {textDecoration:'none', color:'#121A2D'} : {textDecoration:'underline', color:'#121A2D'}}>PROJECTS</a>
+            </Box>
+            <Box>
+            <Button variant="outlined" 
+        onMouseEnter={handleHover} onMouseLeave={handleHoverLeft} 
+        style={hover ? {borderColor:'#121A2D', color:'#121A2D', margin:'10px', fontSize:'15px'}:{color:"#ffffff", background:'#121A2D', margin:'10px', fontSize:'15px'}}>
+        Hit me up
+        </Button>
+        </Box>
+        </Box>
+        </Box>
+        </Box>
+        
+        </Box>
+        <h1 style={{color:'#ffffff', fontSize:50, textAlign:'center', margin:'100px 10px 25px 20px', display:'grid'}}>Weaving Dreams into Web Reality</h1>
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Button variant="outlined" 
+        onMouseEnter={handleHover1} onMouseLeave={handleHoverLeft2}
+        style={hover1 ? {display:'block', color:"#070B2B", background:'#ffffff', margin:'25px auto'} : {display:'block', borderColor:'#ffffff', color:'#ffffff', margin:'25px auto'}}>
+        Dig into my universe
+        </Button>
+    </div>
+        </MediaQuery>
     </>
   )
 }
